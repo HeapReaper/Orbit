619f3e0a5eb731d2d4be9b9711e60c7a25cb7d11
HeapReaper <kelvin@aerobytes.nl>
2025-10-16 15:13:48 +0200

    Updated save button to look more like a button

--- app/(dashboard)/dashboard/components/buttons/Save.tsx
+++ app/(dashboard)/dashboard/components/buttons/Save.tsx
@@ -1,18 +1,22 @@
-import { Save } from "lucide-react";
+import {Save} from "lucide-react";
 
 interface SaveButtonProps {
   onClick?: () => void;
+  loading?: boolean;
 }
 
-export default function SaveButton({ onClick }: SaveButtonProps) {
+export default function SaveButton({ onClick, loading = false }: SaveButtonProps) {
   return (
     <button
-      type="button"
       onClick={onClick}
-      className="text-[var(--primary-color)] hover:text-[var(--hover-color)] transition-colors duration-200"
-      aria-label="Save"
+      disabled={loading}
+      className={`
+          flex items-center gap-2 px-4 py-2 rounded-lg bg-[var(--primary-color)] hover:bg-[var(--hover-color)]
+          text-white font-semibold transition-colors disabled:opacity-50 disabled:cursor-not-allowed
+        `}
     >
-      <Save className="w-6 h-6" />
+      <Save size={18}  />
+      Save
     </button>
   );
 }
