53f5d942c54fa8e81a659fed514c2ef723675018
HeapReaper <kelvin@aerobytes.nl>
2025-10-21 11:51:09 +0200

    Updated data domain of Plausible

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     {/* Plausible analytics */}
     <Script
       defer
-      data-domain="orbit.heapreaper.nl"
+      data-domain="botinorbit.com"
       src="https://analytics.heapreaper.nl/js/script.outbound-links.js"
     />
     <Script id="plausible-init">{`
