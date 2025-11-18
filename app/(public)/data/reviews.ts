3bc23b39af46959cbd4e6dfba4f38c6ec9a2b816
HeapReaper <kelvin@aerobytes.nl>
2025-11-18 21:46:57 +0100

    Added reviews section

---
+++ app/(public)/data/reviews.ts
@@ -0,0 +1,19 @@
+import { ReviewsType } from "@/app/types/reviews";
+
+const reviews: ReviewsType[] = [
+  {
+    name: "TheGhostOfChaos",
+    title: "Server owner",
+    review: "Im using this mod for a few servers.\n" +
+      "\n" +
+      "Its super easy to setup build really simple so everyone can master there server management.\n" +
+      "Compared to other bots i have used before this is by far the fastest easiest and most reliable.\n" +
+      "It does help alot that the Support is active and fast responding in case something is up.\n" +
+      "\n" +
+      "Eventough its under development 6/5",
+    avatar: "/reviews/theghostofchaos.jpg",
+    stars: ["⭐", "⭐", "⭐", "⭐", "⭐"]
+  }
+]
+
+export default reviews;
---
+++ app/types/reviews.ts
@@ -0,0 +1,7 @@
+export interface ReviewsType {
+  name: string;
+  title: string;
+  avatar: string;
+  review: string;
+  stars: string[];
+}
